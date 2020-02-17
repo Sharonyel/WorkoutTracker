78877567513f3ee85e7fc5093c5272b8ccbba5c7
Sharon Yelverton <ladysharonyel@me.com>
2020-02-17 12:34:32 -0500

    updating schema

--- models/exercise.js
+++ models/exercise.js
@@ -6,26 +6,43 @@ const exerciseSchema = new Schema({
   name: {
     type: String,
     trim: true,
+    required: "Enter a workout name"
   },
   type: {
     type: String,
     trim: true,
+    required: "Enter a workout type"
+
   },
   distance: {
     type: Number,
+    required: "Enter the distance"
+
   },
   duration: {
     type: Number,
+    required: "Enter the duration"
+
   },
   weight: {
     type: Number,
+    required: "Enter the weight amount"
+
   },
   sets: {
     type: Number,
+    required: "Enter the number of sets"
+
   },
   reps: {
     type: Number,
+    required: "Enter the number of reps"
+
   },
+  date: {
+      type: Date,
+      default: Date.now
+  }
 
 });
 
