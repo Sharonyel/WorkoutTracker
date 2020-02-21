5776ad8471ddb79238045dbd911385c432610dde
Sharon Yelverton <ladysharonyel@me.com>
2020-02-21 15:23:03 -0500

    fixed undefined duration

--- models/workout.js
+++ models/workout.js
@@ -2,19 +2,14 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-
 const workoutSchema = new Schema({
     day: {
         type: Date,
         default: Date.now
-      },
-    
-  exercises: {
-    type: Array,
-  },
-//   totalDuration: {
-//     type: Number,
-//   },
+    },
+    exercises: {
+        type: Array,
+    },
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
--- public/workout.js
+++ public/workout.js
@@ -5,16 +5,14 @@ async function initWorkout() {
     document
       .querySelector("a[href='/exercise?']")
       .setAttribute("href", `/exercise?id=${lastWorkout._id}`);
-      console.log("Last workout duration:", lastWorkout.totalDuration);
 
-// lastWorkout.totalDuration = 10;
     const workoutSummary = {
       date: formatDate(lastWorkout.day),
       totalDuration: lastWorkout.totalDuration,
       numExercises: lastWorkout.exercises.length,
       ...tallyExercises(lastWorkout.exercises)
     };
-    console.log("workout summary:", workoutSummary);
+
     renderWorkoutSummary(workoutSummary);
   } else {
     renderNoWorkoutText()
@@ -27,8 +25,12 @@ function tallyExercises(exercises) {
       acc.totalWeight = (acc.totalWeight || 0) + curr.weight;
       acc.totalSets = (acc.totalSets || 0) + curr.sets;
       acc.totalReps = (acc.totalReps || 0) + curr.reps;
+      acc.totalDuration = (acc.totalDuration || 0) + curr.duration;
+
     } else if (curr.type === "cardio") {
       acc.totalDistance = (acc.totalDistance || 0) + curr.distance;
+      acc.totalDuration = (acc.totalDuration || 0) + curr.duration;
+
     }
     return acc;
   }, {});
